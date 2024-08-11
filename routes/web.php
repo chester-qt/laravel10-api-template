4493767199fc37623b2167a465bed6f93541ef32
chesterj13 <chester.jazmin@qualitytrade.com>
2024-08-12 06:30:54 +0800

    clean up auth

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,14 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Here is where you can register Web routes for your application.
 |
 */
-
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
-
-require __DIR__.'/auth.php';
